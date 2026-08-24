722c50aae38e56afadecbace5b87be993a4cc298
hstm <2172948307@qq.com>
2026-08-24 20:28:47 +0800

    代码初始化

---
+++ src/main/java/com/bishe/zyf/fireextinguisherselling/FireExtinguisherSellingApplication.java
@@ -0,0 +1,13 @@
+package com.bishe.zyf.fireextinguisherselling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireExtinguisherSellingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FireExtinguisherSellingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bishe/zyf/fireextinguisherselling/FireExtinguisherSellingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bishe.zyf.fireextinguisherselling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireExtinguisherSellingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
